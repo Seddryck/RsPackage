cd603d7b4f1ab27233b1aafe1a4ea3801646c65f
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-06-15 12:49:12 +0200

    Add management of policies

---
+++ RsDeploy/Execution/PolicyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using RsDeploy.ReportingService;
+
+namespace RsDeploy.Execution
+{
+    public class PolicyService : BaseService
+    {
+        public PolicyService(ReportingService2010 reportingService)
+            : base(reportingService)
+        { }
+
+        public void Create(string parent, IEnumerable<Tuple<string, string[]>> securities)
+        {
+            OnInformation($"Configuring policies for '{parent}'");
+
+            var existingRoles = reportingService.ListRoles("All", null);
+
+            var roles = securities.Select(p => p.Item1).Distinct();
+            if (roles.Except(existingRoles.Select(r => r.Name)).Count() > 0)
+                OnError($"Following roles don't exist: {String.Join(", ", roles.Except(existingRoles.Select(r => r.Name)))}");
+
+            var userNames = securities.SelectMany(p => p.Item2).Distinct();
+
+            var policies = new List<Policy>();
+            foreach (var userName in userNames)
+            {
+                var policy = new Policy()
+                {
+                    GroupUserName = userName,
+                    Roles = existingRoles.Where(r => securities.Where(s => s.Item2.Contains(userName)).Select(s => s.Item1).Contains(r.Name)).ToArray()
+                };
+                OnInformation($"Defining policy for {userName} with {policy.Roles.Count()} roles");
+                policies.Add(policy);
+            }
+            OnInformation($"Assigning {policies.Count} policies for '{parent}'");
+
+            reportingService.SetPolicies(parent, policies.ToArray());
+        }
+    }
+}
