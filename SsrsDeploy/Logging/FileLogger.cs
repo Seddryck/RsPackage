9a923ead11bc1cb05dc18c79fefa116962fbb41f
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-08-16 09:27:46 +0200

    Add basic versions of loggers: null, console, flat file

---
+++ SsrsDeploy/Logging/FileLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsrsDeploy.Logging
+{
+    class FileLogger : ILogger
+    {
+        private TextWriter writer;
+        private readonly string path;
+
+        public FileLogger(string path)
+        {
+            this.path = path;
+        }
+
+        public void WriteMessage(object sender, MessageEventArgs eventArgs)
+        {
+            if (writer == null)
+                BuildWriter();
+
+            writer.Write(eventArgs.Time);
+            writer.Write('\t');
+            writer.Write(eventArgs.Level);
+            writer.Write('\t');
+            writer.WriteLine(eventArgs.Message);
+            writer.Flush();
+        }
+
+        private void BuildWriter()
+        {
+            writer = new StreamWriter(this.path);
+        }
+
+        public void Dispose()
+        {
+            writer.Close();
+            writer.Dispose();
+        }
+
+        
+    }
+}
---
+++ SsrsDeploy/Logging/NullLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsrsDeploy.Logging
+{
+    class NullLogger : ILogger
+    {
+        public void Dispose()
+        {
+            return;
+        }
+
+        public void WriteMessage(object sender, MessageEventArgs e)
+        {
+            return;
+        }
+    }
+}
